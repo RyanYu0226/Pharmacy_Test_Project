1d9bb561364e708907c11343a41d88c9832c05d3
RyanYu0000 <ryan_yu@advtekgroup.com.tw>
2025-11-25 12:12:07 +0800

    feat Swagger全面啟動

--- T4U_Pharmacy_API/T4U_Pharmacy_Web_API/Program.cs
+++ T4U_Pharmacy_API/T4U_Pharmacy_Web_API/Program.cs
@@ -71,11 +71,8 @@ builder.Services.AddDbContext<KDAN_TESTContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
